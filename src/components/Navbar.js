47fc3f5a48d3d160750ef3a20ff43f0c575997d1
manikagnish <72827130+manikagnish@users.noreply.github.com>
2021-12-01 22:59:44 +0530

    styled nav and overlay

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,4 +1,4 @@
-import { StyledNavbar } from "./styles/Navbar.styled";
+import { StyledNavbar, StyledNav, Overlay } from "./styles/Navbar.styled";
 
 export default function Navbar() {
   return (
@@ -7,6 +7,29 @@ export default function Navbar() {
       <img src="./images/logo.svg" alt="logo" />
       <img src="./images/icon-cart.svg" alt="shopping cart" />
       <img src="./images/image-avatar.png" alt="user profile" />
+
+      <Overlay />
+
+      <StyledNav>
+        <img src="./images/icon-close.svg" alt="close" />
+        <ul>
+          <li>
+            <a href="/ecommerce-product-page">Collections</a>
+          </li>
+          <li>
+            <a href="/ecommerce-product-page">Men</a>
+          </li>
+          <li>
+            <a href="/ecommerce-product-page">Women</a>
+          </li>
+          <li>
+            <a href="/ecommerce-product-page">About</a>
+          </li>
+          <li>
+            <a href="/ecommerce-product-page">Contact</a>
+          </li>
+        </ul>
+      </StyledNav>
     </StyledNavbar>
   );
 }
--- src/components/styles/Navbar.styled.js
+++ src/components/styles/Navbar.styled.js
@@ -1,6 +1,6 @@
 import styled from "styled-components";
 
-export const StyledNavbar = styled.nav`
+export const StyledNavbar = styled.div`
   display: flex;
   align-items: center;
   background-color: var(--clr-white);
@@ -22,4 +22,43 @@ export const StyledNavbar = styled.nav`
     height: 25px;
     width: 25px;
   }
+`;
+
+export const StyledNav = styled.nav`
+  min-height: 100vh;
+  min-width: 70vw;
+  padding: 2em;
+  background-color: var(--clr-white);
+  position: absolute;
+  left: 0;
+  top: 0;
+  z-index: 10;
+
+  img {
+    margin-bottom: 2em;
+  }
+
+  ul {
+  }
+
+  li {
+    list-style-type: none;
+    margin-bottom: 1em;
+  }
+
+  a {
+    text-decoration: none;
+    color: var(--clr-blue-dark);
+    font-weight: bold;
+  }
+`;
+
+export const Overlay = styled.div`
+  height: 100vh;
+  width: 100vw;
+  position: absolute;
+  top: 0;
+  left: 0;
+  z-index: 5;
+  background-color: rgba(0, 0, 0, 0.75);
 `;
